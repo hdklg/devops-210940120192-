e73d8b9959d76b56afefb7aabb5d5cce1365f958
hdklg_23 <shubhampatil230499@gmail.com>
2022-01-30 14:23:05 +0530

    ans of second question

---
+++ Abc.java
@@ -0,0 +1,6 @@
+public class Abc{
+    public static void main(String[]args){
+        System.out.println("exam is goood");
+        System.out.println("but schedule is very hectic .,, may god help us !!!");
+    }
+}
